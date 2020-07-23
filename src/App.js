cb4719fd9f6afe25fd7420ab6a1a40e460635510
IqraL <IqraLatif159@gmail.com>
2020-07-23 15:09:10 +0100

    inital project setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import {
+  AzureMap,
+  AzureMapsProvider,
+  IAzureMapOptions,
+} from "react-azure-maps";
+import { AuthenticationType } from "azure-maps-control";
+
+const option: IAzureMapOptions = {
+  authOptions: {
+    authType: AuthenticationType.subscriptionKey,
+    subscriptionKey: "Awr0ZM1qOOrNpq8sBxfE68_cpc4DuEuHacPtJvtveCQ",
+  },
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ height: "100vh" }}>
+        <DefaultMap />
+      </div>
     </div>
   );
 }
+
+const DefaultMap: React.FC = () => (
+  <AzureMapsProvider>
+    <AzureMap options={option}></AzureMap>
+  </AzureMapsProvider>
+);
 
 export default App;
